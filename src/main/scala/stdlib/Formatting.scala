13b4cb4d681c6c8e227cbd5c12474f4eb5301f04
cheese.cake <cheese.cake@kakaocorp.com>
2016-08-29 23:26:17 +0900

    add Formatting practice

--- src/main/scala/stdlib/Formatting.scala
+++ src/main/scala/stdlib/Formatting.scala
@@ -11,7 +11,7 @@ object Formatting extends FlatSpec with Matchers with org.scalaexercises.definit
     */
   def placedInFormatFormatting(res0: String) {
     val s = "Hello World"
-    "Application %s".format(s) should be(res0)
+    "Application %s".format(s) should be("Application Hello World")
   }
 
   /** Character Literals can be a single character:
@@ -23,8 +23,8 @@ object Formatting extends FlatSpec with Matchers with org.scalaexercises.definit
     //format(a) is a string format, meaning the "%c".format(x)
     //will return the string representation of the char.
 
-    "%c".format(a) should be(res0)
-    "%c".format(b) should be(res1)
+    "%c".format(a) should be("a")
+    "%c".format(b) should be("B")
   }
 
   /** Character Literals can be an escape sequence, including octal or hexidecimal:
@@ -35,17 +35,17 @@ object Formatting extends FlatSpec with Matchers with org.scalaexercises.definit
     val e = '\"'
     val f = '\\'
 
-    "%c".format(c) should be(res0)
-    "%c".format(d) should be(res1)
-    "%c".format(e) should be(res2)
-    "%c".format(f) should be(res3)
+    "%c".format(c) should be("a")
+    "%c".format(d) should be("a")
+    "%c".format(e) should be("\"")
+    "%c".format(f) should be("\\")
   }
 
   /** Formatting can also include numbers:
     */
   def includingNumbersFormatting(res0: String) {
     val j = 190
-    "%d bottles of beer on the wall" format j - 100 should be(res0)
+    "%d bottles of beer on the wall" format j - 100 should be("90 bottles of beer on the wall")
   }
 
   /** Formatting can be used for any number of items, like a string and a number:
@@ -54,7 +54,7 @@ object Formatting extends FlatSpec with Matchers with org.scalaexercises.definit
     val j = 190
     val k = "vodka"
 
-    "%d bottles of %s on the wall".format(j - 100, k) should be(res0)
+    "%d bottles of %s on the wall".format(j - 100, k) should be("90 bottles of vodka on the wall")
   }
 
 }
